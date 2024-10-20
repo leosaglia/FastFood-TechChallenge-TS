f94913b65b55cbc77f63f6a280b856013ff38234
Léo Saglia <leosaglia@hotmail.com>
2024-10-20 19:52:44 -0300

    subir postgres e configurar tabelas com prisma

--- src/adapter/driver/nest/app.module.ts
+++ src/adapter/driver/nest/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
+import { ProductController } from './controllers/product.controller'
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [ProductController],
   providers: [],
 })
 export class AppModule {}
